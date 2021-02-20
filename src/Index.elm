3f0151c0a7cb3e024d20b74bccc802d32d102a96
unknown <gummi.bear456aaaaa@gmail.com>
2021-02-21 01:48:19 +0900

    create until the token is obtained

--- src/Index.elm
+++ src/Index.elm
@@ -13,11 +13,21 @@ import Task exposing (..)
 import Url.Parser as Url exposing ((</>), (<?>))
 import Url.Parser.Query as Query
 
-port setStorage : (String) -> Cmd msg
+
+port setStorage : String -> Cmd msg
+
+
 port getStorage : String -> Cmd msg
-port resStorage : ((String, String) -> msg) -> Sub msg
+
+
+port resStorage : (( String, String ) -> msg) -> Sub msg
+
+
 port getLocation : String -> Cmd msg
-port resLocation : ((String, String) -> msg) -> Sub msg
+
+
+port resLocation : (( String, String ) -> msg) -> Sub msg
+
 
 main : Program () Model Msg
 main =
@@ -35,9 +45,18 @@ main =
 
 type alias Model =
     { dataState : DataState
+    , progress : Progress
     , clientSecret : String
     , code : String
     }
+
+
+type Progress
+    = InCheckToken
+    | InAuthorize
+    | InGetToken
+    | InLoaded Token
+    | InFinish
 
 
 type DataState
@@ -48,8 +67,9 @@ type DataState
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model Init "" ""
-    , getStorage "token")
+    ( Model Init InCheckToken "" ""
+    , getStorage "token"
+    )
 
 
 
@@ -58,14 +78,11 @@ init _ =
 
 type Msg
     = InputClientSecret String
-    | Send
-    | CheckToken
-    | ReceiveCheckToken (Result Http.Error CheckTokenData)
-    | Authorize
     | GetToken
-    | Receive (String, String)
-    | GetLocation
-    | ReceiveLocation (String)
+    | Authorize
+    | Receive ( String, String )
+    | ReceiveLocation ( String, String )
+    | PostCreated (Result Http.Error Token)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -78,68 +95,79 @@ update msg model =
             , Cmd.none
             )
 
-        Send ->
+        Authorize ->
             ( { model
                 | dataState = Waiting
               }
             , Nav.load getAuthUrl
             )
 
-        CheckToken ->
-            ( { model
-                | dataState = Waiting
-              }
-            , Cmd.none
-            )
-
-        Authorize ->
-            ( { model
-                | dataState = Waiting
-                , code = authorize model
-              }
-            , if model.code == "" then
-                Cmd.none
-
-              else
-                Task.perform GetToken
-            )
-
         GetToken ->
             ( { model
                 | dataState = Waiting
               }
-            , Cmd.none{-tockenの取得-}
+            , getToken model
             )
-        Receive  (_, val) ->
-            (model , checkToken val)
 
-        ReceiveLocation  (paramCode, paramState) ->
-            ({model | code = paramCode}, checkParam paramCode paramState)
+        Receive ( _, val ) ->
+            ( model, checkToken val )
+
+        ReceiveLocation ( paramCode, paramState ) ->
+            if paramCode /= "" && paramState == "Pd3mSwgs" then
+                ( { model | progress = InCheckToken, code = paramCode }, Cmd.none )
+
+            else
+                ( { model | progress = InAuthorize }, Cmd.none )
+
+        PostCreated (Ok post) ->
+            ( { model | progress = InLoaded post }
+            , Cmd.none
+            )
+
+        PostCreated (Err error) ->
+            ( model
+            , Cmd.none
+            )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  resStorage Receive
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  resLocation ReceiveLocation
+    Sub.batch
+        [ resStorage Receive
+        , resLocation ReceiveLocation
+        ]
 
 
 checkToken : String -> Cmd Msg
 checkToken token =
-  if (token == "") then getLocation ""  else Cmd.none
+    if token == "" then
+        getLocation ""
 
-checkParam : String -> String -> Cmd Msg
-checkParam code state =
-  if (code /= "" && state == "Pd3mSwgs") then Cmd.none  else Nav.load getAuthUrl
+    else
+        Cmd.none
 
-getToken 
-getToken 
-      Http.post
-        { url = "http://localhost:5019/posts"
-        , body = Http.jsonBody (newPostEncoder post)
-        , expect = Http.expectJson PostCreated postDecoder
+
+
+{- token完了 -}
+
+
+getToken : Model -> Cmd Msg
+getToken model =
+    Http.post
+        { url = "https://qiita.com/api/v2/access_tokens"
+        , body = Http.jsonBody (makeRequestParameter model)
+        , expect = Http.expectJson PostCreated tokenDecoder
         }
+
+
+makeRequestParameter : Model -> Encode.Value
+makeRequestParameter model =
+    Encode.object
+        [ ( "client_id", Encode.string "190908c64f149924c34c7e381cf3faed0a6236c4" )
+        , ( "client_secret", Encode.string model.clientSecret )
+        , ( "code", Encode.string model.code )
+        ]
+
 
 maybeStringToString : Maybe String -> String
 maybeStringToString str =
@@ -168,15 +196,33 @@ getAuthUrl =
 view : Model -> Html Msg
 view model =
     div []
-        [ Html.form [ onSubmit GetToken ]
-            [ input
-                [ onInput InputClientSecret
-                , value model.clientSecret
-                ]
-                []
-            , button [ disabled ((model.dataState == Waiting) || String.isEmpty (String.trim model.clientSecret)) ]
-                [ text "Submit" ]
-            ]
+        [ case model.progress of
+            InCheckToken ->
+                div [] []
+
+            InAuthorize ->
+                Html.form [ onSubmit Authorize ]
+                    [ div [] [ text "この機能を使用するには、Qiitaとの連携が必要です。\n以下のボタンをクリックし、認証ページで許可してください。" ]
+                    , button [ disabled (model.dataState == Waiting) ]
+                        [ text "Submit" ]
+                    ]
+
+            InGetToken ->
+                Html.form [ onSubmit GetToken ]
+                    [ input
+                        [ onInput InputClientSecret
+                        , value model.clientSecret
+                        ]
+                        []
+                    , button [ disabled ((model.dataState == Waiting) || String.isEmpty (String.trim model.clientSecret)) ]
+                        [ text "Submit" ]
+                    ]
+
+            InLoaded token ->
+                div [] []
+
+            InFinish ->
+                div [] []
         ]
 
 
@@ -184,6 +230,11 @@ view model =
 --DATA
 
 
-type alias CheckTokenData =
-    { test : Bool
+type alias Token =
+    { token : String
     }
+
+
+tokenDecoder : Decoder Token
+tokenDecoder =
+    D.map Token (D.field "token" D.string)
